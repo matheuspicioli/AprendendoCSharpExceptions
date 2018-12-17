f199e6acbb746362e4b2cf8c21c95eacb215038e
Matheus Picioli <matheus.picioli98@gmail.com>
2018-12-17 13:48:16 -0200

    Classes base que será usado no curso como exemplo

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -6,7 +6,42 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine("Não é possível dividir por zero");
+                Console.WriteLine(erro.StackTrace);
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Metodo()
+        {
+            TestaDivisao(2);
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por "
+                + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            //Console.WriteLine(conta.Numero);
+            return numero / divisor;
         }
     }
 }
